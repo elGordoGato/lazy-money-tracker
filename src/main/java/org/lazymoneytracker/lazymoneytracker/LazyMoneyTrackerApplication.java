3de2ba5d22a3e456ff75b562c95f3a93f75b0323
elGordoGato <elGordoGato95@gmail.com>
2024-01-03 13:49:08 +0700

    Initial commit

---
+++ src/main/java/org/lazymoneytracker/lazymoneytracker/LazyMoneyTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.lazymoneytracker.lazymoneytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazyMoneyTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazyMoneyTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/lazymoneytracker/lazymoneytracker/LazyMoneyTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lazymoneytracker.lazymoneytracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LazyMoneyTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
